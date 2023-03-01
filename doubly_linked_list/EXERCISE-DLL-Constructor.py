f6ed5ab1dbaebf65211448701e609ea46f50957f
Mohammad Moeini <mbm6448@psu.edu>
2023-03-01 16:09:40 -0500

    dll before .get

--- doubly_linked_list/EXERCISE-DLL-Constructor.py
+++ doubly_linked_list/EXERCISE-DLL-Constructor.py
@@ -1,23 +1,27 @@
-# class Node:
-    ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
-        
-# class DoublyLinkedList:
-    ## WRITE DLL CONSTRUCTOR HERE ##
-    #                              #
-    #                              #
-    #                              #
-    #                              #
-    ################################
-  
+class Node:
+    def __init__(self,value) -> None:
+        self.value = value
+        self.next = None
+        self.prev = None
 
+    
+        
+class DoublyLinkedList:
+    def __init__(self,value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    def print_list(self):
+        temp =self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
 
 
 my_doubly_linked_list = DoublyLinkedList(7)
+
+my_doubly_linked_list.print_list()
 
 print('Head:', my_doubly_linked_list.head.value)
 print('Tail:', my_doubly_linked_list.tail.value)
